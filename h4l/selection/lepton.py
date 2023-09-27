158398212f070c9da2929a022f12a524fefe2ed0
Philip Daniel Keicher <philip.daniel.keicher@cern.ch>
2023-09-27 17:06:06 +0200

    add linting

--- h4l/selection/lepton.py
+++ h4l/selection/lepton.py
@@ -9,14 +9,18 @@ from h4l.util import IF_NANO_V9, IF_NANO_V10
 np = maybe_import("numpy")
 ak = maybe_import("awkward")
 
+
 @selector(
-    uses={f"Electron.{var}"
-          for var in {"pt", "eta", "deltaEtaSC",
+    uses=(
+        {
+            f"Electron.{var}"
+            for var in {"pt", "eta", "deltaEtaSC",
                       "dxy", "dz", "sip3d"}
-          } | {
+        } | {
             IF_NANO_V9("Electron.mvaFall17V2Iso"),
             IF_NANO_V10("Electron.mvaHZZIso"),
-          },
+        }
+    ),
     exposed=False,
     sandbox=dev_sandbox("bash::$CF_BASE/sandboxes/venv_columnar.sh"),
 )
@@ -32,30 +36,52 @@ def electron_selection(
     def return_cuts():
 
         era = self.config_inst.campaign.x.year
-        if era == 2017 or era == 2018 :
+        if era == 2017 or era == 2018:
             if self.config_inst.campaign.x.version < 10:
-                BDT = events.Electron.mvaFall17V2Iso     # Using 2017 WP and training (ElectronMVAEstimatorRun2Fall17IsoV2Values) since this is the only one available in Run2 UL nanoAODs.
-            else :
+                # Using 2017 WP and training (ElectronMVAEstimatorRun2Fall17IsoV2Values) since this is the only one available in Run2 UL nanoAODs. noqa
+                BDT = events.Electron.mvaFall17V2Iso
+            else:
                 BDT = events.Electron.mvaHZZIso
-            if self.config_inst.campaign.x.preUL : # pre-UL WP for Run II (miniAOD branch: Run2_CutBased_BTag16)
+
+            # pre-UL WP for Run II (miniAOD branch: Run2_CutBased_BTag16)
+            if self.config_inst.campaign.x.preUL:  
                 # print("This is preUL!")
                 # from IPython import embed; embed()
-                return ((pt<=10.) &     ((fSCeta<0.8                   & BDT > 0.85216885148) | \
-                                        (fSCeta>=0.8 & fSCeta<1.479 & BDT > 0.82684550976) | \
-                                        (fSCeta>=1.479                & BDT > 0.86937630022))) \
-                        | ((pt>10.) &  ((fSCeta<0.8                   & BDT > 0.98248928759) | \
-                                        (fSCeta>=0.8 & fSCeta<1.479 & BDT > 0.96919224579) | \
-                                        (fSCeta>=1.479                & BDT > 0.79349796445)))
+                return (
+                    (
+                        (pt <= 10.) & (
+                            (fSCeta < 0.8 & BDT > 0.85216885148) |
+                            (fSCeta >= 0.8 & fSCeta < 1.479 & BDT > 0.82684550976) |
+                            (fSCeta >= 1.479 & BDT > 0.86937630022)
+                        )
+                    ) |
+                    (
+                        (pt > 10.) & (
+                            (fSCeta < 0.8 & BDT > 0.98248928759) |
+                            (fSCeta >= 0.8 & fSCeta < 1.479 & BDT > 0.96919224579) |
+                            (fSCeta >= 1.479 & BDT > 0.79349796445))
+                    )
+                )
 
-            else: # UL WP (miniAOD branch Run2_CutBased_UL)
+            else:  # UL WP (miniAOD branch Run2_CutBased_UL)
                 # print("This is not preUL!")
                 # from IPython import embed; embed()
-                return ((pt<=10.) &     (((fSCeta<0.8)                   & (BDT > 0.9128577458)) | \
-                                        ((fSCeta>=0.8) & (fSCeta<1.479) & (BDT > 0.9056792368)) | \
-                                        ((fSCeta>=1.479)                & (BDT > 0.9439440575)))) \
-                        | ((pt>10.) &  (((fSCeta<0.8)                   & (BDT > 0.1559788054)) | \
-                                        ((fSCeta>=0.8) & (fSCeta<1.479) & (BDT > 0.0273863727)) | \
-                                        ((fSCeta>=1.479)                & (BDT > -0.5532483665))))
+                return (
+                    (
+                        (pt <= 10.) & (
+                            ((fSCeta < 0.8) & (BDT > 0.9128577458)) |
+                            ((fSCeta >= 0.8) & (fSCeta < 1.479) & (BDT > 0.9056792368)) |
+                            ((fSCeta >= 1.479) & (BDT > 0.9439440575))
+                        )
+                    ) |
+                    (
+                        (pt > 10.) & (
+                            ((fSCeta < 0.8) & (BDT > 0.1559788054)) |
+                            ((fSCeta >= 0.8) & (fSCeta < 1.479) & (BDT > 0.0273863727)) |
+                            ((fSCeta >= 1.479) & (BDT > -0.5532483665))
+                        )
+                    )
+                )
         return ak.ones_like(events.Electron.pt)
     default_mask = (
         (pt > min_pt) &
@@ -68,45 +94,48 @@ def electron_selection(
     sorted_idx = ak.argsort(events.Electron.pt, axis=1, ascending=False)
     # filter unselected electron indices
     selected_electron_idx = sorted_idx[default_mask[sorted_idx]]
-    
-
 
     return events, SelectionResult(
-        objects = {
+        objects={
             "Electron": {
-                "Electron": selected_electron_idx
-            }
-        }
+                "Electron": selected_electron_idx,
+            },
+        },
     )
+
 
 @selector(
     uses=({
         f"Muon.{var}" for var in [
             # four momenta information
-            "pt", "eta", "phi", "mass", 
+            "pt", "eta", "phi", "mass",
             # quality criteria
             "isGlobal", "isStandalone", "isTracker", "nStations", "nTrackerLayers",
             # impact parameters
             "dxy", "dz", "sip3d",
             # IDs
             "tightId", "mvaId", "highPtId",
-            #isolation
+            # isolation
             "pfRelIso03_all",
         ]}
     ),
     exposed=False,
 )
-def muon_selector(self: Selector, events: ak.Array, **kwargs) -> tuple[ak.Array, SelectionResult]:
+def muon_selector(
+    self: Selector,
+    events: ak.Array,
+    **kwargs,
+) -> tuple[ak.Array, SelectionResult]:
     min_pt = 15
     max_eta = 2.4
     selected_muon_mask = (
         # Global or Tracker Muon
         (events.Muon.isGlobal | events.Muon.isTracker) &
         # Discard Standalone Muon tracks if reconstructed in muon system only
-        (~events.Muon.isStandalone | (events.Muon.nTrackerLayers >0)) &
-        # WIP: Discard muons with muonBestTrackType==2 even if they are global or tracker muons
+        (~events.Muon.isStandalone | (events.Muon.nTrackerLayers > 0)) &
+        # WIP: Discard muons with muonBestTrackType==2 even if they are global or tracker muons # noqa
         # --> muonBestTrackType not available?
-        (events.Muon.pt > min_pt) & 
+        (events.Muon.pt > min_pt) &
         (abs(events.Muon.eta) < max_eta) &
         (events.Muon.dxy < 0.5) & (events.Muon.dz < 1.0) &
         (abs(events.Muon.sip3d) < 4) &
@@ -118,9 +147,9 @@ def muon_selector(self: Selector, events: ak.Array, **kwargs) -> tuple[ak.Array,
     # filter unselected muon indices
     muon_idx = sorted_idx[selected_muon_mask[sorted_idx]]
     return events, SelectionResult(
-        objects= {
+        objects={
             "Muon": {
-                "Muon": muon_idx
-            }
-        }
+                "Muon": muon_idx,
+            },
+        },
     )
